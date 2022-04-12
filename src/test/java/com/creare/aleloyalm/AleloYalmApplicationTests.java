255d5f2f69875eefec0328b1ca1cef55e5753d68
felipecampos94 <felipecamposlv@gmail.com>
2022-04-12 17:14:47 -0300

    first commit

---
+++ src/test/java/com/creare/aleloyalm/AleloYalmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creare.aleloyalm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AleloYalmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
